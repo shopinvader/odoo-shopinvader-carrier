b9fa21e9304782f86a90f999d846a77863fa8f10
Sébastien BEAU <sebastien.beau@akretion.com>
2019-06-24 18:27:47 +0200

    [IMP] start adding module for dropoff site

---
+++ shopinvader_dropoff_site/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Akretion (http://www.akretion.com)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopinvader Dropoff site",
+    "summary": "Shopinvader Dropoff site",
+    "version": "10.0.0.0.0",
+    "category": "e-commerce",
+    "website": "https://akretion.com",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["shopinvader_delivery_carrier", "delivery_dropoff_site"],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
