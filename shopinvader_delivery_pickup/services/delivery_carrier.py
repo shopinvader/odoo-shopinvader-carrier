2e98d82262f7e9acdd791701eebe2671c07102bb
hparfr <raph@hpar.fr>
2021-06-08 16:26:41 +0200

    shopinvader_delivery_pickup: black, isort, prettier

--- shopinvader_delivery_pickup/services/delivery_carrier.py
+++ shopinvader_delivery_pickup/services/delivery_carrier.py
@@ -16,7 +16,9 @@ class DeliveryCarrierService(Component):
         return res
 
     def _prepare_carrier(self, carrier, no_price=False):
-        res = super(DeliveryCarrierService, self)._prepare_carrier(carrier, no_price=no_price)
+        res = super(DeliveryCarrierService, self)._prepare_carrier(
+            carrier, no_price=no_price
+        )
         if carrier.with_dropoff_site:
             res["type"] = "pickup"
         return res
