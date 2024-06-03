jbustio/odoo-shopinvader

[ADD][10.0] add delivery price simulation for cart

shopinvader_delivery_carrier/services/cart.py
@@ -3,81 +3,15 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
 import logging
+from contextlib import contextmanager
 
-from odoo.addons.component.core import AbstractComponent, Component
+from odoo.addons.base_rest.components.service import to_int
+from odoo.addons.component.core import Component
 from odoo.exceptions import UserError
-from odoo.tools import float_round
 from odoo.tools.translate import _
 
 _logger = logging.getLogger(__name__)
-
-
-class AbstractSaleService(AbstractComponent):
-    _inherit = "shopinvader.abstract.sale.service"
-
-    def _convert_shipping(self, cart):
-        res = super(AbstractSaleService, self)._convert_shipping(cart)
-        selected_carrier = {}
-        if cart.carrier_id:
-            carrier = cart.carrier_id
-            selected_carrier = {
-                "id": carrier.id,
-                "name": carrier.name,
-                "description": carrier.description,
-            }
-        res.update(
-            {
-                "amount": {
-                    "tax": cart.shipping_amount_tax,
-                    "untaxed": cart.shipping_amount_untaxed,
-                    "total": cart.shipping_amount_total,
-                },
-                "selected_carrier": selected_carrier,
-            }
-        )
-        return res
-
-    def _convert_amount(self, sale):
-        """
-        Inherit to add amounts without shipping prices included
-        :param sale: sale.order recordset
-        :return: dict
-        """
-        result = super(AbstractSaleService, self)._convert_amount(sale)
-        # Remove the shipping amounts for originals amounts
-        shipping_amounts = self._convert_shipping(sale).get("amount", {})
-        tax = result.get("tax", 0) - shipping_amounts.get("tax", 0)
-        untaxed = result.get("untaxed", 0) - shipping_amounts.get("untaxed", 0)
-        total = result.get("total", 0) - shipping_amounts.get("total", 0)
-        precision = sale.currency_id.decimal_places
-        result.update(
-            {
-                "tax_without_shipping": float_round(tax, precision),
-                "untaxed_without_shipping": float_round(untaxed, precision),
-                "total_without_shipping": float_round(total, precision),
-            }
-        )
-        return result
-
-    def _prepare_carrier(self, carrier):
-        return {
-            "id": carrier.id,
-            "name": carrier.name,
-            "description": carrier.description,
-            "price": carrier.price,
-        }
-
-    def _get_available_carrier(self, cart):
-        return [
-            self._prepare_carrier(carrier)
-            for carrier in cart._get_available_carrier()
-        ]
-
-    def _is_item(self, line):
-        res = super(AbstractSaleService, self)._is_item(line)
-        return res and not line.is_delivery
 
 
 class CartService(Component):
@@ -106,6 +40,56 @@ class CartService(Component):
             self._set_carrier(cart, params["carrier"]["id"])
             return self._to_json(cart)
 
+    @contextmanager
+    def _simulate_delivery_cost(self, cart):
+        """
+        Change the env mode (draft) to avoid real update on the partner.
+        Then, restore the cart with previous values.
+        As the delivery_set function create a new sale.order.line related to
+        the current cart, we have to remove it (by _delivery_unset).
+        So if the cart already had a delivery line, this one will be removed!
+        :param cart: sale.order recordset
+        :return:
+        """
+        cart_values = cart._convert_to_write(cart._cache)
+        cart_values.pop("order_line", None)
+        # should be the same
+        partner = self.partner or cart.partner_id
+        partner_values = partner._convert_to_write(partner._cache)
+        had_delivery_line = any(cart.order_line.mapped("is_delivery"))
+        with self.env.do_in_draft():
+            yield cart
+            # Remove the delivery line
+            cart._delivery_unset()
+            # Restore values
+            cart.update(cart_values)
+            partner.update(partner_values)
+            # Re-set delivery (if previously set)
+            if had_delivery_line:
+                cart.delivery_set()
+
+    def get_cart_price_by_country(self, **params):
+        """
+        Get the cart price by country/zip.
+        The cart is not updated with the given country. The change is done
+        only in memory.
+        :param params: dict
+        :return: dict
+        """
+        country = self._load_country(params)
+        zip_code = self._load_zip_code(params)
+        with self._simulate_delivery_cost(self._get()) as cart:
+            # Edit country and zip
+            # Even if some info are not provided, we have to fill them
+            # Ex: if the zip code is not provided, we have to do the
+            # simulation with an empty zip code on the partner. Because his
+            # current zip could be related to another country and simulate
+            # a wrong price.
+            cart.partner_id.update({"country_id": country.id, "zip": zip_code})
+            cart.delivery_set()
+            json_cart = self._to_json(cart)
+        return json_cart
+
     # Validator
     def _validator_apply_delivery_method(self):
         return {
@@ -125,7 +109,38 @@ class CartService(Component):
     def _validator_get_delivery_methods(self):
         return {}
 
+    def _validator_get_cart_price_by_country(self):
+        """
+
+        :return: dict
+        """
+        return {
+            "country_id": {
+                "coerce": to_int,
+                "required": True,
+                "type": "integer",
+            },
+            "zip_code": {"required": False, "type": "string"},
+        }
+
     # internal methods
+    def _load_country(self, params):
+        """
+        Load the country from given params
+        :param params: dict
+        :return: res.country recordset
+        """
+        country_id = params.pop("country_id", 0)
+        return self.env["res.country"].browse(country_id)
+
+    def _load_zip_code(self, params):
+        """
+        Load the country from given params
+        :param params: dict
+        :return: str
+        """
+        return params.pop("zip_code", "")
+
     def _add_item(self, cart, params):
         res = super(CartService, self)._add_item(cart, params)
         self._unset_carrier(cart)

shopinvader/odoo-shopinvader-carrier

shopinvader_delivery_carrier/services/cart.py
@@ -2,6 +2,7 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo.addons.base_rest.components.service import to_int
 from odoo.addons.component.core import Component
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
@@ -10,14 +11,25 @@ from odoo.tools.translate import _
 class CartService(Component):
     _inherit = "shopinvader.cart.service"
 
-    def get_delivery_methods(self):
+    def get_delivery_methods(self, **params):
         """
-            This service will return all possible delivery methods for the
-            current cart
-        :return:
+        This service will return all possible delivery methods for the
+        current cart (depending on country/zip)
+        The cart is not updated with the given country/zip. The change is done
+        only in memory.
+        :param params: dict
+        :return: dict
         """
         cart = self._get()
-        return self._get_available_carrier(cart)
+        country = self._load_country(params)
+        zip_code = self._load_zip_code(params)
+        if country or zip_code:
+            cart = cart.with_context(
+                delivery_force_country_id=country.id,
+                delivery_force_zip_code=zip_code,
+            )
+        result = self._get_available_carrier(cart)
+        return result
 
     def apply_delivery_method(self, **params):
         """
@@ -50,9 +62,33 @@ class CartService(Component):
         }
 
     def _validator_get_delivery_methods(self):
-        return {}
+        return {
+            "country_id": {
+                "coerce": to_int,
+                "required": False,
+                "type": "integer",
+            },
+            "zip_code": {"required": False, "type": "string"},
+        }
 
     # internal methods
+    def _load_country(self, params):
+        """
+        Load the country from given params
+        :param params: dict
+        :return: res.country recordset
+        """
+        country_id = params.pop("country_id", 0)
+        return self.env["res.country"].browse(country_id)
+
+    def _load_zip_code(self, params):
+        """
+        Load the country from given params
+        :param params: dict
+        :return: str
+        """
+        return params.pop("zip_code", "")
+
     def _add_item(self, cart, params):
         res = super()._add_item(cart, params)
         self._unset_carrier(cart)