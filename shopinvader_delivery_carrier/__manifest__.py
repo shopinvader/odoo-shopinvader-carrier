7436614e6f86cac796d7ab37877d86668653503e
Florian Mounier <paradoxxx.zero@gmail.com>
2026-04-13 11:27:57 +0200

    [MIG] shopinvader_delivery_carrier: Migration to 18.0

--- shopinvader_delivery_carrier/__manifest__.py
+++ shopinvader_delivery_carrier/__manifest__.py
@@ -3,9 +3,8 @@
 
 {
     "name": "Shopinvader Delivery Carrier",
-    "summary": """
-        ShopInvader logic for delivery carriers.""",
-    "version": "16.0.1.0.0",
+    "summary": "ShopInvader logic for delivery carriers.",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Shopinvader",
     "website": "https://github.com/shopinvader/odoo-shopinvader-carrier",
@@ -13,5 +12,5 @@
         "delivery",
     ],
     "data": [],
-    "installable": False,
+    "installable": True,
 }
